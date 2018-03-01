b513669fe3071e0e7094d7515a388f7399963a57
Olga <ol.gure@gmail.com>
2018-03-01 19:08:03 +0300

    refactor: change project structure, write logs to file

--- logger.py
+++ logger.py
@@ -6,3 +6,7 @@ fmt = logging.Formatter('%(asctime)s: [ %(message)s ]', '%m/%d/%Y %I:%M:%S %p')
 file = logging.FileHandler('log.txt')
 file.setFormatter(fmt)
 logger.addHandler(file)
+
+# console = logging.StreamHandler()
+# console.setFormatter(fmt)
+# logger.addHandler(console)
