08b174fb68b2a2e6785d4df154d12bd4c80c4665
Olga <ol.gure@gmail.com>
2018-03-05 17:58:15 +0300

    exclude load_matrix() from vectorizer class, include it to utils

--- vectorizer.py
+++ vectorizer.py
@@ -165,10 +165,3 @@ class HashingTfIdfVectorizer:
             'opts': opts
         }
         np.savez(save_path, **data)
-
-    @staticmethod
-    def load_matrix(load_path) -> Tuple[sp.sparse.csr_matrix, Dict]:
-        loader = np.load(load_path)
-        matrix = sp.sparse.csr_matrix((loader['data'], loader['indices'],
-                                loader['indptr']), shape=loader['shape'])
-        return matrix, loader['opts'].item(0)
