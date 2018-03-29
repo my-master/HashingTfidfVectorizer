146f2d58664cc3c7373146d33c0d0052f03c7f42
Olga <ol.gure@gmail.com>
2018-03-29 15:23:01 +0300

    fix: batch generation, refactor: minor

--- iterators/simple_iterator.py
+++ iterators/simple_iterator.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple, Any, Generator
 
 from logger import logger
 
@@ -7,20 +7,19 @@ class SimpleIterator:
     def __init__(self, data, batch_size=None):
         self.data = data
         self.batch_size = batch_size
-        self.doc_index = self.get_doc_ids(data)
+        self.doc2index = self.get_doc_ids()
 
-    def get_doc_ids(self, data: List[str]):
-        ids = [i for i in range(len(data))]
-        return ids
+    def get_doc_ids(self):
+        return list(range(len(self.data)))
 
-    def read_batch(self, batch_size=1000):
+    def read_batch(self, batch_size=1000) -> Generator[Tuple[List[str], List[Any]], Any, None]:
         _batch_size = self.batch_size or batch_size
-        batches = [self.doc_index[i:i + _batch_size] for i in
-                   range(0, len(self.doc_index), _batch_size)]
+        batches = [self.doc2index[i:i + _batch_size] for i in
+                   range(0, len(self.doc2index), _batch_size)]
         len_batches = len(batches)
 
         for i, doc_ids in enumerate(batches):
             logger.info(
                 "Processing batch # {} of {} ({} documents)".format(i, len_batches, len(doc_ids)))
-            docs = self.data[doc_ids[0]: doc_ids[-1]]
+            docs = [self.data[j] for j in doc_ids]
             yield docs, doc_ids
