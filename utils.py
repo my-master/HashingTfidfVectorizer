61972803e92717b787264b872108c7472a770090
Olga <ol.gure@gmail.com>
2018-04-02 16:26:02 +0300

    refactor: include load_tfidf_matrix() in the vectorizer methods

--- utils.py
+++ utils.py
@@ -1,17 +1,6 @@
-from typing import Tuple, Dict
-
 from sklearn.utils import murmurhash3_32
-import scipy as sp
-from scipy import sparse
-import numpy as np
 
 
 def hash(token, hash_size):
     return murmurhash3_32(token, positive=True) % hash_size
 
-
-def load_tfidf_matrix(load_path) -> Tuple[sp.sparse.csr_matrix, Dict]:
-    loader = np.load(load_path)
-    matrix = sp.sparse.csr_matrix((loader['data'], loader['indices'],
-                                   loader['indptr']), shape=loader['shape'])
-    return matrix, loader['opts'].item(0)
