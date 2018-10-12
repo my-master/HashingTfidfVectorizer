1087b36c40d5f6ddc6502552d0355cab145a09f2
Olga Gureenkova <ol.gure@gmail.com>
2018-10-12 13:49:06 +0300

    fix: ru_run

--- ru_run.py
+++ ru_run.py
@@ -7,24 +7,30 @@ STOPWORDS = stopwords.words('russian')
 from iterators.sqlite_iterator import SQLiteDataIterator
 from tokenizers.ru_tokenizer import RussianTokenizer
 
+from tokenizers.stream_spacy_tokenizer import StreamSpacyTokenizer
+
 from vectorizer import HashingTfIdfVectorizer
 from logger import logger
 
-DATA_PATH = '/media/olga/Data/projects/ODQA/data/wiki_db/ruwiki.db'
-SAVE_PATH = '/media/olga/Data/projects/ODQA/data/wiki_db/ruwiki_tfidf_matrix.npz'
+DATA_PATH = '/media/olga/Data/projects/DeepPavlov/download/odqa/enwiki_test.db'
+SAVE_PATH = '/media/olga/Data/projects/DeepPavlov/download/odqa/enwiki_test_1.npz'
 
 iterator = SQLiteDataIterator(DATA_PATH, batch_size=1000)
-vectorizer = HashingTfIdfVectorizer(iterator,
-                                    tokenizer=RussianTokenizer(ngram_range=(1, 2), lemmas=True,
-                                                               stopwords=STOPWORDS))
-
+vectorizer = HashingTfIdfVectorizer(iterator, tokenizer=RussianTokenizer(ngram_range=[1, 2],
+                                                                         lemmas=True,
+                                                                         stopwords=STOPWORDS))
 start_time = time.time()
 
 try:
     vectorizer.fit()
     vectorizer.save(SAVE_PATH)
-    logger.info("Completed successfully in {} s.".format(time.time() - start_time))
+    # DEBUG
+    # vectorizer.load(SAVE_PATH)
+    # vectorizer.transform(data)
 except Exception as e:
-    logger.info("Completed with exceptions in {} s.".format(time.time() - start_time))
     logger.exception(e)
     raise
+
+# DEBUG
+# vectorizer.load(SAVE_PATH)
+# logger.info("Completed in {} s.".format(time.time() - start_time))
